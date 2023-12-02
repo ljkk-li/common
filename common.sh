db0fe3adb40eebd5cfe79bb35b2c731949afcea5
ljkk-li <51225555+ljkk-li@users.noreply.github.com>
2023-12-02 23:01:52 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -271,7 +271,7 @@ src-git danshui1 https://github.com/281677160/openwrt-package.git;${SOURCE}
 EOF
 ./scripts/feeds update -a
 cat >>"feeds.conf.default" <<-EOF
-src-git helloworld https://github.com/fw876/helloworld;main
+src-git helloworld https://github.com/fw876/helloworld.git
 src-git passwall3 https://github.com/xiaorouji/openwrt-passwall-packages;main
 EOF
 
